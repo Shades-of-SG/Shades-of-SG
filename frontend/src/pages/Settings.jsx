97eee63000707c43bf6997e12aa1e93682bbd10f
Solitice-debug <250466Q@mymail.nyp.edu.sg>
2026-07-08 14:16:48 +0800

    fixed settings

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,6 +1,3 @@
-import CreatorPageShell from '../components/CreatorPageShell'
-import SectionCard from '../components/SectionCard'
-
 /*
 TODO - Lia
 
@@ -40,15 +37,21 @@ export default function Settings() {
       />
       <section className="two-column">
         <Link to="/settings/profile" style={{ textDecoration: 'none' }}>
-          <SectionCard title="Profile"><p>Manage username and email.</p></SectionCard>
+          <SectionCard title="Profile">
+            <p>Manage username and email.</p>
+          </SectionCard>
         </Link>
         <Link to="/settings/account-security" style={{ textDecoration: 'none' }}>
-          <SectionCard title="Account & Security"><p>Manage password and enable 2FA.</p></SectionCard>
+          <SectionCard title="Account & Security">
+            <p>Manage password and enable 2FA.</p>
+          </SectionCard>
         </Link>
         <Link to="/settings/data-privacy" style={{ textDecoration: 'none' }}>
-          <SectionCard title="Data & Privacy"><p>Delete your account.</p></SectionCard>
+          <SectionCard title="Data & Privacy">
+            <p>Delete your account.</p>
+          </SectionCard>
         </Link>
       </section>
-    </CreatorPageShell>
+    </div>
   )
 }
